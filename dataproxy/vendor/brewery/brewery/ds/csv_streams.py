945bd1b0a486df64f254b2fbc7bb8b525d69e36e
Ross Jones <rossdjones@gmail.com>
2012-06-12 09:38:28 +0100

    Simple fix for encoding problem.
    Problem with encoding seems to have been related to the assumption that
    it is possible to encode anything retrieved from the internet as UTF8
    which isn't the case, particularly when the charset is something like
    latin1 (128 from ascii plus some european chars up to 255).
    
    After looking through the messytables code to find out how it handles
    the encoding it appears that the UTF8Recoder there sets the charset
    encoding to ignore utf8 chars that it can't convert (most of the latin1
    charset >128, or so it appears).  As this is a preview, and after
    discussion with DR, we decided that it is the most effective resolution
    at present. Have copied the UTF8Recoder from messytables over the old
    one that was part of the brewery code here.
    
    So now when encountering non-utf8 charsets where it fails to convert
    individual chars it will instead ignore those characters.  The option
    does still exist to specify the encoding on the URL, so where something
    like the spanish_example (from the messy tables test folder which is at
    https://github.com/okfn/messytables/tree/master/horror) has chars dropped
    it is possible to add encoding=latin1 to the end of the url and receive all
    of the special characters.

--- dataproxy/vendor/brewery/brewery/ds/csv_streams.py
+++ dataproxy/vendor/brewery/brewery/ds/csv_streams.py
@@ -3,23 +3,23 @@ import codecs
 import cStringIO
 import base
 
-class UTF8Recoder(object):
+class UTF8Recoder:
     """
     Iterator that reads an encoded stream and reencodes the input to UTF-8
-
-    From: <http://docs.python.org/lib/csv-examples.html>
     """
-    def __init__(self, f, encoding=None):
-        if encoding:
-            self.reader = codecs.getreader(encoding)(f)
-        else: # already unicode so just return f
-            self.reader = f
+    def __init__(self, f, encoding):
+        self.reader = codecs.getreader(encoding)(f, 'ignore')
 
     def __iter__(self):
         return self
 
     def next(self):
-        return self.reader.next().encode('utf-8')
+        line = self.reader.readline()
+        if not line or line == '\0':
+            raise StopIteration
+        result = line.encode("utf-8")
+        return result
+
 
 class UnicodeWriter:
     """
@@ -43,7 +43,7 @@ class UnicodeWriter:
                 new_row.append(value.encode("utf-8"))
             else:
                 new_row.append(unicode(value))
-                
+
         self.writer.writerow(new_row)
         # Fetch UTF-8 output from the queue ...
         data = self.queue.getvalue()
@@ -61,60 +61,60 @@ class UnicodeWriter:
 
 class CSVDataSource(base.DataSource):
     """docstring for ClassName
-    
+
     Some code taken from OKFN Swiss library.
     """
-    def __init__(self, resource, read_header = True, dialect = None, encoding=None, detect_encoding = False, 
+    def __init__(self, resource, read_header = True, dialect = None, encoding=None, detect_encoding = False,
                 detect_header = False, sample_size = 200, **reader_args):
         """Creates a CSV data source stream.
-        
+
         :Attributes:
             * resource: file name, URL or a file handle with CVS data
-            * read_header: flag determining whether first line contains header or not. 
+            * read_header: flag determining whether first line contains header or not.
                 ``True`` by default.
-            * encoding: source character encoding, by default no conversion is performed. 
+            * encoding: source character encoding, by default no conversion is performed.
             * detect_encoding: read sample from source and determine whether source is UTF8 or not
             * detect_headers: try to determine whether data source has headers in first row or not
             * sample_size: maximum bytes to be read when detecting encoding and headers in file. By
                 default it is set to 200 bytes to prevent loading huge CSV files at once.
-        
+
         Note: avoid auto-detection when you are reading from remote URL stream.
-        
+
         """
         self.read_header = read_header
         self.encoding = encoding
         self.detect_encoding = detect_encoding
         self.detect_header = detect_header
-        
+
         self._autodetection = detect_encoding or detect_header
-        
+
         self.sample_size = sample_size
         self.resource = resource
         self.reader_args = reader_args
         self.reader = None
         self.dialect = dialect
-        
+
         self.close_file = False
-        
+
         self._fields = None
-        
+
     def initialize(self):
         """Initialize CSV source stream:
-        
+
         #. perform autodetection if required:
             #. detect encoding from a sample data (if requested)
             #. detect whether CSV has headers from a sample data (if requested)
         #.  create CSV reader object
         #.   read CSV headers if requested and initialize stream fields
-        
+
         """
 
         self.file, self.close_file = base.open_resource(self.resource)
 
         handle = None
-        
+
         if self._autodetection:
-            
+
             sample = self.file.read(self.sample_size)
 
             # Encoding test
@@ -131,8 +131,8 @@ class CSVDataSource(base.DataSource):
                 self.read_header = sniffer.has_header(sample)
 
             self.file.seek(0)
-            
-        
+
+
         if not handle:
             handle = UTF8Recoder(self.file, self.encoding)
 
@@ -141,7 +141,7 @@ class CSVDataSource(base.DataSource):
                 dialect = csv.get_dialect(self.dialect)
             else:
                 dialect = self.dialect
-                
+
             self.reader_args["dialect"] = dialect
 
         self.reader = csv.reader(handle, **self.reader_args)
@@ -150,7 +150,7 @@ class CSVDataSource(base.DataSource):
         if self.read_header:
             fields = self.reader.next()
             self._fields = base.fieldlist(fields)
-        
+
     def finalize(self):
         if self.file and self.close_file:
             self.file.close()
@@ -164,7 +164,7 @@ class CSVDataSource(base.DataSource):
         if not self._fields:
             raise ValueError("Fields are not initialized in CSV source")
         return self._fields
-        
+
     def set_fields(self, fields):
         self._fields = fields
 
@@ -172,15 +172,15 @@ class CSVDataSource(base.DataSource):
 
 
 class CSVDataTarget(base.DataTarget):
-    def __init__(self, resource, write_headers = True, truncate = True, encoding = "utf-8", 
+    def __init__(self, resource, write_headers = True, truncate = True, encoding = "utf-8",
                 dialect = None, **kwds):
         """Creates a CSV data target
-        
+
         :Attributes:
             * resource: target object - might be a filename or file-like object
             * write_headers: write field names as headers into output file
             * truncate: remove data from file before writing, default: True
-            
+
         """
         self.resource = resource
         self.write_headers = write_headers
@@ -200,9 +200,9 @@ class CSVDataTarget(base.DataTarget):
 
         self.file, self.close_file = base.open_resource(self.resource, mode)
 
-        self.writer = UnicodeWriter(self.file, encoding = self.encoding, 
+        self.writer = UnicodeWriter(self.file, encoding = self.encoding,
                                     dialect = self.dialect, **self.kwds)
-        
+
         if self.write_headers:
             self.writer.writerow(self.field_names)
 
