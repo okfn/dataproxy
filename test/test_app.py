7f13598df1461822183124765a1e6f062dc905ae
Ross Jones <rossdjones@gmail.com>
2012-06-12 10:13:02 +0100

    Fixed broken tests
    Replaced XLS/CSV files which are no longer hosted at democracyfarm and
    pointed them to appropriate data on datahub.io

--- test/test_app.py
+++ test/test_app.py
@@ -13,20 +13,20 @@ class TestDataProxy(object):
 
         # Create application
         self.wsgiapp = JsonpDataProxy(100000)
-        self.app = TestApp(self.wsgiapp) 
-        
+        self.app = TestApp(self.wsgiapp)
+
         # Define named requests for easier (re)cofiguration and reuse.
         self.requests = {
                 "no_type": "url=http://foo.com/foo",
                  "unknown_type": "url=http://foo.com/foo.undefined",
-                 "valid_csv": "url=http://democracyfarm.org/f/ckan/foo.csv",
+                 "valid_csv": "url=https://commondatastorage.googleapis.com/ckannet-storage/2011-11-24T112025/AfTerFibre_21nov2011.csv",
                  "valid_csv_limit": {
-                            "url": "http://democracyfarm.org/f/ckan/foo.csv",
+                            "url": "https://commondatastorage.googleapis.com/ckannet-storage/2011-11-24T112025/AfTerFibre_21nov2011.csv",
                             "max-results": 3,
                             "format": "json"
                             },
-                 "csv_json": "url=http://democracyfarm.org/f/ckan/foo.csv&format=json",
-                 "valid_xls": "url=http://democracyfarm.org/f/ckan/foo.xls",
+                 "csv_json": "url=https://commondatastorage.googleapis.com/ckannet-storage/2011-11-24T112025/AfTerFibre_21nov2011.csv&format=json",
+                 "valid_xls": "url=http://oee.nrcan.gc.ca/corporate/statistics/neud/dpa/tablestrends2/id_ca_28_e.xls",
                  "untyped_csv": {
                         "url": "http://openeconomics.net/store/8d7d4770-e1d1-11db-9f7e-00145101c316/data",
                         "type": "csv"
@@ -35,17 +35,17 @@ class TestDataProxy(object):
                 }
     def get(self, request_name):
         request = self.requests[request_name]
-        
+
         if type(request) == str:
             request_str = request
         else:
             pairs = ["%s=%s" % item for item in request.items()]
             request_str = "&".join(pairs)
-        
+
         result = self.app.get('/?' + request_str)
-            
+
         return result
-        
+
     def test_no_params(self):
         res = self.app.get('/')
         assert '"error"' in res, res
@@ -53,7 +53,7 @@ class TestDataProxy(object):
 
     def acceptable_response(self, body):
         return re.search('"data": \[\[.*\]\]', body)
-        
+
     def test_resource_type_support(self):
         res = self.get("no_type")
         assert 'Could not determine the resource type' in res, res
@@ -63,7 +63,7 @@ class TestDataProxy(object):
 
         res = self.get("valid_csv")
         assert 'Resource type not supported' not in res, res
-    
+
         res = self.get("valid_xls")
         assert 'Resource type not supported' not in res, res
 
@@ -77,14 +77,14 @@ class TestDataProxy(object):
 
         res = self.get("untyped_csv")
         assert self.acceptable_response(res.body), res
-        
+
         res = self.get("csv_json")
         assert self.acceptable_response(res.body), res
         assert 'callback' not in res, res
         jres = json.loads(res.body)
         assert 'data' in jres
         assert 'url' in jres
-        
+
     def test_redirect(self):
         res = self.get("redirect_csv")
         assert self.acceptable_response(res.body), res
