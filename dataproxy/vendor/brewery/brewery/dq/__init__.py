ea93112826ae8977f595e6e1ab6a676ee232fe99
Rufus Pollock <rufus.pollock@okfn.org>
2013-01-19 18:28:40 +0000

    [#10,refactor][m]: remove code obsolete transform and brewery code - fixes #10.
    Last part of refactor as part of #10 to use dataconverters: remove the old libraries and code we were using.

--- dataproxy/vendor/brewery/brewery/dq/__init__.py
+++
@@ -1,2 +0,0 @@
-from base import *
-from field_statistics import *
--- dataproxy/vendor/brewery/brewery/utils.py
+++
@@ -1,8 +0,0 @@
-"""Brewery handy utilities"""
-
-class IgnoringDictionary(dict):
-    """Simple dictionary extension that will ignore any keys of which values are empty (None/False)"""
-    def setnoempty(self, key, value):
-        """Set value in a dictionary if value is not null"""
-        if value:
-            self[key] = value
